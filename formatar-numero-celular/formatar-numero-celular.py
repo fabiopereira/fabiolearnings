2c662dd0fd33f9ac091ea1ea3ff5493334624c8e
Fabio Pereira <fabiopereira.me@gmail.com>
2023-11-19 19:12:46 -0300

    adicionar_nove_ao_celular
    adicionar_nove_ao_celular

--- formatar-numero-celular/formatar-numero-celular.py
+++ formatar-numero-celular/formatar-numero-celular.py
@@ -2,7 +2,7 @@ import pandas as pd
 from phonenumbers import PhoneNumberFormat, parse as phone_parse, format_number as phone_format
 from io import StringIO
 
-numeros = """Número
+numeros = """ColNum
 +5585988001677
 85 85460364
 85 99221-8211
@@ -16,8 +16,23 @@ numeros = """Número
 85999202078
 """
 
+def adicionar_nove_ao_celular(numero):
+    # Verifica se o número já possui um "9" após o DDD
+    if len(numero) == 13 and numero[0] == '+':
+        return numero[:5] + '9' + numero[5:]
+
+    return numero
+
+# Testando adicionar_nove_ao_celular
+# numero_celular = "+551143247524"  # Substitua pelo número de celular desejado
+# novo_numero_celular = adicionar_nove_ao_celular(numero_celular)
+# print(f"Número original: {numero_celular}")
+# print(f"Novo número: {novo_numero_celular}")
+
+
 df = pd.read_csv(StringIO(numeros), sep=';')
 
-df['Número'] = df['Número'].apply(lambda n: phone_format(phone_parse(n, 'BR'), PhoneNumberFormat.E164))
+df['ColNum'] = df['ColNum'].apply(lambda n: phone_format(phone_parse(n, 'BR'), PhoneNumberFormat.E164))
+df['ColNum'] = df['ColNum'].apply(lambda n: adicionar_nove_ao_celular(n))
 
 print(df)
